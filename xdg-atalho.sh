57a0cd8e913af52c869c54ee0813089659b5210a
dougpess <dougpess@hotmail.com>
2020-03-27 22:03:12 -0300

    Update xdg-atalho.sh

--- xdg-atalho.sh
+++ xdg-atalho.sh
@@ -8,20 +8,24 @@ echo "Icon=$4" >> $DESKTOP_FILE
 echo "Type=Application" >> $DESKTOP_FILE
 echo "Categories=$5;" >> $DESKTOP_FILE
 
-if [[ -n $6 ]]; then
-        echo -e "Baixando ícone... \c"
-        (wget -q -N --trust-server-names $6 && xdg-icon-resource install --novendor --size 64 $4.png) &>/dev/null
+function check () {
         if [[ $? -eq 0 ]]; then
                 echo "[Sucesso]"
             else
                 echo "[Falhou]"
         fi
+}
+
+if [[ -n $6 ]]; then
+        echo -e "Baixando ícone do aplicativo... \c"
+        (wget -q -N --trust-server-names $6 && xdg-icon-resource install --novendor --size 64 $4.png) &>/dev/null
+        check
 fi
 
-echo -e "Criando atalho... \c"
+echo -e "Criando atalho de menu... \c"
 (xdg-desktop-menu install --novendor $DESKTOP_FILE) &>/dev/null
-if [[ $? -eq 0 ]]; then
-        echo "[Sucesso]"
-    else
-        echo "[Falhou]"
-fi
+check
+
+echo -e "Criando atalho de área de trabalho... \c"
+(xdg-desktop-icon install --novendor $DESKTOP_FILE) &>/dev/null
+check
